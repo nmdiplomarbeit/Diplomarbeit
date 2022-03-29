369bdd5bd56deda19a45f021ba17f2890e597b4a
matteohoelli <mattweiss@tsn.at>
2022-03-29 23:46:27 +0200

    WindowBuilder Test
    Matteo Höllrigl

---
+++ test/src/test/test.java
@@ -0,0 +1,57 @@
+package test;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class test extends JFrame {
+
+	private JPanel contentPane;
+	private JButton btnNewButton;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					test frame = new test();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public test() {
+		initialize();
+	}
+	private void initialize() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		btnNewButton = new JButton("TEST");
+		btnNewButton.addActionListener(new BtnNewButtonActionListener());
+		btnNewButton.setBounds(10, 10, 85, 21);
+		contentPane.add(btnNewButton);
+	}
+	private class BtnNewButtonActionListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+}
